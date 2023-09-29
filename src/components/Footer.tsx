6a36bb565ad39ddd82d491dbabfae291d805e7cd
firzhaardhia <firzharamadhan27@gmail.com>
2023-09-29 12:30:31 +0700

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="pb-8 pt-4 mx-auto text-sm font-extralight sm:w-[59%] max-w-[1000px]">
+    <footer className="pb-8 pt-4 mx-auto text-sm font-extralight sm:w-[70%] w-[90%] max-w-[1000px]">
       <p>this is not your local disk C, or D. this is</p>
       <p className="font-bold">localdiskzha. <span className="font-extralight">All Rights Reserved &copy;</span></p>
     </footer>
