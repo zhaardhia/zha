6c75ff20a5c62ca849a0144fe9b4bc1a4c0f3a94
firzhaardhia <firzharamadhan27@gmail.com>
2024-01-19 18:44:29 +0700

    change color gradient in company experience

--- src/components/profile/Experience.tsx
+++ src/components/profile/Experience.tsx
@@ -10,16 +10,34 @@ import Link from 'next/link'
 
 const Experience = () => {
   return (
-    <Accordion type="single" collapsible>
+    <Accordion type="multiple">
       <AccordionItem value="item-1">
-        <AccordionTrigger>
+        <AccordionTrigger className="no-underline">
           <div className="flex flex-col gap-2 text-start">
-            <p className="text-lg no-underline">Software Engineer at <span className="text-[#2cb67d]">Mayar</span></p>
+            {/* <p className="text-lg no-underline">Software Engineer at <span className="text-[#0C54EC] hover:border-b-[2px] hover:border-[#EB1C7C]">May<span className="text-[#EB1C7C]">ar</span></span></p> */}
+            <p className="text-lg no-underline">Software Engineer at {" "}
+              <span className="text-[#0C54EC]"
+                style={{
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                  backgroundImage: "linear-gradient(90deg, #0C54EC, #EB1C7C)"
+                }}
+              >Mayar</span>
+            </p>
             <p className="text-sm text-slate-300">Mar 2023 - Present</p>
           </div>
         </AccordionTrigger>
         <AccordionContent>
-          <a href='https://mayar.id' target='_blank' className="underline text-slate-600">Mayar.id</a>
+          <a href='https://mayar.id' target='_blank' className="underline"
+            style={{
+              backgroundClip: "text",
+              WebkitBackgroundClip: "text",
+              color: "transparent",
+              backgroundImage: "linear-gradient(90deg, #0C54EC, #EB1C7C)",
+              textDecoration: "underline"
+            }}
+          >mayar.id</a>
           <ol className="flex flex-col gap-3 text-lg job-description text-slate-600">
             <FadeBox delay={`${0 + 1}00ms`}>
               <li>Developed and maintain a scalable, high-performing, and user-friendly web application for digital payment solution & commerce for growing business using React + Redux, GraphQL, Node, and Redis</li>
@@ -36,12 +54,29 @@ const Experience = () => {
       <AccordionItem value="item-2">
         <AccordionTrigger>
           <div className="flex flex-col gap-2 text-start">
-            <p className="text-lg">Frontend Engineer Intern at <span className="text-[#2cb67d]">Campaign.com</span></p>
+            <p className="text-lg">Frontend Engineer Intern at {" "}
+            {/* <span className="text-[#0d6efd]">Campaign.com</span> */}
+              <span className="text-[#0C54EC]"
+                style={{
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                  backgroundImage: "linear-gradient(90deg, #0d6efd, #00d1ff)"
+                }}
+              >Campaign.com</span>
+            </p>
             <p className="text-sm text-slate-300">Jan 2023 - Jul 2023</p>
           </div>
         </AccordionTrigger>
         <AccordionContent>
-          <a href='https://campaign.com' target='_blank' className="underline text-slate-600">Campaign.com</a>
+          <a href='https://campaign.com' target='_blank' className="underline text-slate-600"
+            style={{
+              backgroundClip: "text",
+              WebkitBackgroundClip: "text",
+              color: "transparent",
+              backgroundImage: "linear-gradient(90deg, #0d6efd, #00d1ff)"
+            }}
+          >campaign.com</a>
           <ol className="flex flex-col gap-3 text-lg job-description text-slate-600">
             <FadeBox delay={`${0 + 1}00ms`}>
               <li>Successfully created & improved feature for admin-side</li>
@@ -64,12 +99,29 @@ const Experience = () => {
       <AccordionItem value="item-3">
         <AccordionTrigger>
           <div className="flex flex-col gap-2 text-start">
-            <p className="text-lg">Software Engineer Intern at <span className="text-[#2cb67d]">IKI Karunia Indonesia (Anabatic Digital Raya's Subsidiaries)</span></p>
+            <p className="text-lg">Software Engineer Intern at {" "}
+              {/* <span className="text-[#f14651]">IKI Karunia Indonesia (Anabatic Digital Raya's Subsidiaries)</span> */}
+              <span className="text-[#0C54EC]"
+                style={{
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                  backgroundImage: "linear-gradient(90deg, #f14651, #000000)"
+                }}
+              >IKI Karunia Indonesia (Anabatic Digital Raya's Subsidiaries)</span>
+            </p>
             <p className="text-sm text-slate-300">Feb 2022 - Feb 2023</p>
           </div>
         </AccordionTrigger>
         <AccordionContent>
-          <a href='https://v2.ikimodal.app' target='_blank' className="underline text-slate-600">IKI Modal</a>
+          <a href='https://ikimodal.com' target='_blank' className="underline text-slate-600"
+            style={{
+              backgroundClip: "text",
+              WebkitBackgroundClip: "text",
+              color: "transparent",
+              backgroundImage: "linear-gradient(90deg, #f14651, #000000)"
+            }}
+          >ikimodal.com</a>
           <ol className="flex flex-col gap-3 text-lg job-description text-slate-600">
             <FadeBox delay={`${0 + 1}00ms`}>
               <li>Collaborate closely in the squad to deliver feature</li>
